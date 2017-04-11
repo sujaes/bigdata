163ae2caa6d1c43af27f8383fdc043283ffb96a3
장수제/201311240 <sujaes@naver.com>
2017-04-11 18:04:18 +0900

    mongodb

---
+++ src/ds_save_mongo_test.js
@@ -0,0 +1,10 @@
+MongoDB shell version v3.4.2
+connecting to: mongodb://127.0.0.1:27017
+MongoDB server version: 3.4.2
+Server has startup warnings: 
+2017-04-11T16:51:01.717+0900 I CONTROL  [initandlisten] 
+2017-04-11T16:51:01.717+0900 I CONTROL  [initandlisten] ** WARNING: Access control is not enabled for the database.
+2017-04-11T16:51:01.718+0900 I CONTROL  [initandlisten] **          Read and write access to data and configuration is unrestricted.
+2017-04-11T16:51:01.720+0900 I CONTROL  [initandlisten] 
+> cd..cd..^C
+bye
