0912fc9017ebd2c4941185eab4a13b0aa1284efb
장수제/201311240 <sujaes@naver.com>
2017-05-02 17:51:42 +0900

    info

---
+++ mylib3.py
@@ -0,0 +1,8 @@
+def getKey(keyPath):
+    d=dict()
+    f=open(keyPath,'r')
+    for line in f.readlines():
+        row=line.split('=')
+        row0=row[0]
+        d[row0]=row[1].strip()
+    return d
